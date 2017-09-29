f3c75395b694666b963fcc13d27f41aefb47e4ff
Brahian Giraldo Medina <brahian609@hotmail.com>
2017-09-29 12:22:14 -0500

    Consumir servicio con axios

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,53 +1,51 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.orgit statusg/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div id="container" v-for="(value, key) in cryptos">
+      <span class="left">{{ key }}</span>
+      <span class="right">{{ value.USD }}</span>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  import axios from 'axios'
+
+  export default {
+    name: 'hello',
+    data: () => ({
+      cryptos: [],
+      errors: []
+    }),
+
+    created () {
+      axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,IOT&tsyms=USD')
+        .then(response => {
+          this.cryptos = response.data
+          console.log('response')
+          console.log(response)
+        })
+        .catch(e => {
+          this.errors.push(e)
+        })
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+  div#container {
+    background: white;
+    width: 70%;
+    margin: 0 auto 4px auto;
+    padding: 1em;
+    box-shadow: 1px 1px 0 lightgray;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  span.left {
+    font-weight: bold;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+  span.right {
+    float: right;
+  }
 </style>
